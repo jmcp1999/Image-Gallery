c691b2053438ac2b43759610528a521b139d5768
Jose Manuel Cruz Pacheco <cruzpachecojosemanuel@gmail.com>
2024-07-10 10:57:48 +0200

    first commit

---
+++ src/test/java/com/Image/Gallery/ImageGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Image.Gallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
